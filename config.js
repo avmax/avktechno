348f8efb9848e761fb6e7a9e2f522662f5f01110
avmax <avmax.web@gmail.com>
2018-10-14 20:13:59 +0300

    change config for bento

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const isProd = env.NODE_ENV && env.NODE_ENV.indexOf('prod') !== -1;
 const config = () => {
   const cfg = {
     db: {
-      name: env.DB_NAME || 'avktechno',
+      name: env.DB_NAME || 'bento_club',
       username: env.DB_USER || 'root',
-      password: env.DB_PASSWORD || '12345',
+      password: env.DB_PASSWORD || '7757',
       host: env.DB_HOST || 'localhost',
     },
-    baseURL: env.BASE_URL || 'http://90.156.142.46',
-    domainURL: env.DOMAIN_URL || 'http://avktechno.com',
+    baseURL: env.BASE_URL || 'http://90.156.157.222',
+    domainURL: env.DOMAIN_URL || 'http://bento-club.ru',
     server: {
       port: +env.SERVER_PORT || 4251,
     },
     client: {
-      port: +env.CLIENT_PORT || 80,
+      port: +env.CLIENT_PORT || 4252,
     },
     isProd: env.IS_PROD || isProd,
   }
